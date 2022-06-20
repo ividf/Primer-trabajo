8bb31d35eec3e4ae79900f83e88c96b77b36a0da
Alvarogarfer <104387766+Alvarogarfer@users.noreply.github.com>
2022-06-20 22:52:41 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 "use strict";
 
+// Leer JSON
+
+async function getData(url) {							
+	const respuesta = await fetch (url)
+    const data = await respuesta.json()
+    console.log(data);
+    return data 
+}							
+
+const data = getData("./quiz.json")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
