76ce9492fb82021ca6d0d1a1b4ad19a7839868d0
ividf <ivandiazfraga@gmail.com>
2022-06-24 19:49:54 +0200

    casi final

--- script.js
+++ script.js
@@ -2,19 +2,47 @@
 const queryBotones=document.querySelectorAll(".boton")
 let numeroPregunta ;
 let puntos=0
+const queryPregunta=document.querySelector("#pregunta")
 let queryPuntos = document.querySelector("#puntos span")
-console.log(queryPuntos);
-
+let queryPuntosSpam=document.querySelector("#puntos")
+let queryBoton1Apagar=document.querySelector("#boton1")
+let queryBoton2Apagar=document.querySelector("#boton2")
+let queryBoton3Apagar=document.querySelector("#boton3")
+let queryBoton4Apagar=document.querySelector("#boton4")
 let acumulacion=[]
 
+
+
+//////////////////////////////////////////////
+
 function numeroAleatorio(){
-        let num = Math.floor(Math.random()*49)
+    
+    
+    let num = Math.floor(Math.random()*49)
+    
+            
+            if(acumulacion.includes(num))
+            {
+                console.log("se repite "+num);
+                
+                num = Math.floor(Math.random()*49)
+            }
+            else
+            {
+                acumulacion.push(num)
+
+            }
+        
+        
+        console.log(acumulacion);
     
         return numeroPregunta=num
         
     }
+    //////////////////////////////////////////////////
 
     async function getData(ruta){
+
         const response = await fetch(ruta)
     
         const data = await response.json()
@@ -37,25 +65,29 @@ function numeroAleatorio(){
         numeroAleatorio()
         let pregunta =arrayJson[numeroPregunta]
         console.log(pregunta);
+        
         pintarPregunta(pregunta)
+
     
 
     }
     ////////////////////////////////
+    
     preguntaAleatoria()
+
     
     
-    const queryPregunta=document.querySelector("#pregunta")
 
     
     /////////////////////////////////////
-    function pintarPregunta({question,answers}){
+    function pintarPregunta({question,answers})
+    {
         console.log(question,answers);
         queryPregunta.textContent=question
         console.log(queryPregunta);
+        
         for(let i=0;i<queryBotones.length;i++){
-            console.log(queryBotones[i]);
-            console.log(answers[i]);
+            
             queryBotones[i].textContent=answers[i]
         }
 
@@ -65,33 +97,91 @@ function numeroAleatorio(){
 
     async function presBoton(evento){
         
+        
         let textoRespuestas=evento.target.textContent
-        let arrayJson =await data//aqui tengo un array con el json
+        let arrayJson =await data
         let pregunta =arrayJson[numeroPregunta]
         console.log(pregunta.correct);
-
-
-
+    
 
         if(pregunta.correct===textoRespuestas)
         {
+            console.log(pregunta.correct);
+            console.log(acumulacion.length);
             puntos++
             queryPuntos.textContent=puntos
+                    
         }
-        preguntaAleatoria()
-        
-        
+        if(acumulacion.length>10){
 
+            queryPuntosSpam.textContent="PUNTUACION FINAL: " +puntos+"/50"
+            apagarBoton()
+            
+        }    
+        preguntaAleatoria()  
+    }
     
-        
+    for(let i=0;i<queryBotones.length;i++)
+    {
+        queryBotones[i].addEventListener("click",presBoton)
+    }
+    
+    
+    //////////////////
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    function apagarBoton()
+    {
+        queryBoton1Apagar.disabled=true
+        queryBoton2Apagar.disabled=true
+        queryBoton3Apagar.disabled=true
+        queryBoton4Apagar.disabled=true
+    }
+    function encenderBoton()
+    {
+        queryBoton1Apagar.disabled=false
+        queryBoton2Apagar.disabled=false
+        queryBoton3Apagar.disabled=false
+        queryBoton4Apagar.disabled=false
         
     }
-   for(let i=0;i<queryBotones.length;i++){
-       queryBotones[i].addEventListener("click",presBoton)
-   }
+
     
-  
-    
+
+
+
+
 
 
     
